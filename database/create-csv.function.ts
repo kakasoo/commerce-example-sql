43f69624de387cd31ceb06eecf47a8b0ad3ce171
kakasoo <kscodebase@gmail.com>
2023-09-28 00:41:58 +0900

    feat : database 초기 세팅 시 데이터를 삽입하기 위한 create-csv-function file & guest.csv file

---
+++ database/create-csv.function.ts
@@ -0,0 +1,35 @@
+import { uuid } from "uuidv4";
+import { fakerEN_GB } from "@faker-js/faker";
+import * as fs from "fs";
+
+function createCsv() {
+    const NUMBER_OF_ROWS = 10000;
+
+    const csvContent = new Array(NUMBER_OF_ROWS)
+        .fill(0)
+        .map((_, line) => {
+            const id = uuid();
+            const email = fakerEN_GB.internet.email();
+            const username = fakerEN_GB.person.fullName();
+            const password_hash = fakerEN_GB.internet.password();
+            const createdAt = fakerEN_GB.date.between({
+                from: new Date("2020-01-01"),
+                to: new Date("2022-12-31"),
+            });
+            return [
+                id,
+                email,
+                username,
+                username,
+                password_hash,
+                createdAt,
+                createdAt,
+                null,
+            ].join(",");
+        })
+        .join("\n");
+
+    fs.writeFileSync("initial.csv", csvContent);
+}
+
+createCsv();
