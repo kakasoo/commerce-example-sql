e6b8e9651bc0bb33c3a89b3b781e7fe87e469659
kakasoo <kscodebase@gmail.com>
2023-10-04 00:00:32 +0900

    feat : 반드시 1개 이상의 세션이 존재하도록 강제하기

--- database/create-csv.function.ts
+++ database/create-csv.function.ts
@@ -45,27 +45,21 @@ async function init() {
       const userCreatedAt = generateRandomDate(from, to);
       const userUpdatedAt = generateRandomDate(userCreatedAt, to);
 
-      /**
-       * 90% 확률로, 유저는 회원가입 전 이미 이 사이트를 방문했을 것이라고 가정한다.
-       * 방문한 유저는 최소 1번에서 100번 사이의 방문을 하다가 회원가입한 것으로 간주한다.
-       */
-      if (generateRandomNumber(1, 100) <= 90) {
-        const temp = new Array(generateRandomNumber(1, 100))
-          .fill(0)
-          .map((_, line) => {
-            const guestId = uuid();
-            /**
-             * 유저가 생성되기 전까지만 비회원 활동한 것으로 간주한다.
-             * 실제로는 유저가 로그아웃 상태로도 돌아다닐 수 있기 때문에 그 이후의 시간 값도 있다.
-             * 단, 로그인할 때마다 게스트에서 유저의 uuid를 찾아 넣어주기 때문에 결국은 전부 찾아낼 수 있다.
-             */
-            const createdAt = generateRandomDate(from, userCreatedAt);
-            // return [guestId, userId, createdAt].join(",");
-            return `"${guestId}","${userId}","${createdAt}"`;
-          });
+      const temp = new Array(generateRandomNumber(1, 100))
+        .fill(0)
+        .map((_, line) => {
+          const guestId = uuid();
+          /**
+           * 유저가 생성되기 전까지만 비회원 활동한 것으로 간주한다.
+           * 실제로는 유저가 로그아웃 상태로도 돌아다닐 수 있기 때문에 그 이후의 시간 값도 있다.
+           * 단, 로그인할 때마다 게스트에서 유저의 uuid를 찾아 넣어주기 때문에 결국은 전부 찾아낼 수 있다.
+           */
+          const createdAt = generateRandomDate(from, userCreatedAt);
+          // return [guestId, userId, createdAt].join(",");
+          return `"${guestId}","${userId}","${createdAt}"`;
+        });
 
-        guestCsvContents.push(...temp);
-      }
+      guestCsvContents.push(...temp);
 
       return `"${userId}","${email}","${username}","${password_hash}","${userCreatedAt}","${userUpdatedAt}"`;
     })
